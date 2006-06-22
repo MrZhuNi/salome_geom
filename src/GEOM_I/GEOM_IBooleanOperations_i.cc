c420fe5dcc92a8b8d0d06c407507c0f757ed428e
ptv <ptv@opencascade.com>
2006-06-22 10:53:50 +0000

    add check before array declaration

--- src/GEOM_I/GEOM_IBooleanOperations_i.cc
+++ src/GEOM_I/GEOM_IBooleanOperations_i.cc
@@ -154,7 +154,8 @@ GEOM::GEOM_Object_ptr GEOM_IBooleanOperations_i::MakePartition
 
   //Get the materials
   aLen = theMaterials.length();
-  aMaterials = new TColStd_HArray1OfInteger (1, aLen);
+  if ( aLen )
+    aMaterials = new TColStd_HArray1OfInteger (1, aLen);
   for (ind = 0; ind < aLen; ind++) {
     aMaterials->SetValue(ind+1, theMaterials[ind]);
   }
