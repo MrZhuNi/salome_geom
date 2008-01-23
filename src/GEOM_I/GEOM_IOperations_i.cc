de662f5fde9c29a442e4a13c5965cbc87ac66b4e
jfa <jfa@opencascade.com>
2008-01-23 15:00:14 +0000

    NPAL18604: Pb. of performance with MakeVertex. Fixed memory leak.

--- src/GEOM_I/GEOM_IOperations_i.cc
+++ src/GEOM_I/GEOM_IOperations_i.cc
@@ -1,22 +1,23 @@
 // Copyright (C) 2005  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
 // CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
-// 
+//
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either 
+// License as published by the Free Software Foundation; either
 // version 2.1 of the License.
-// 
-// This library is distributed in the hope that it will be useful 
-// but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+//
+// This library is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public  
-// License along with this library; if not, write to the Free Software 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 //
 // See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
+
 #include "GEOM_IOperations_i.hh"
 
 #include "GEOM_Engine.hxx"
@@ -34,9 +35,10 @@
  *  default constructor:
  */
 //=============================================================================
-
-GEOM_IOperations_i::GEOM_IOperations_i(PortableServer::POA_ptr thePOA, GEOM::GEOM_Gen_ptr theEngine, ::GEOM_IOperations* theImpl)
-:SALOME::GenericObj_i( thePOA ), _impl(theImpl), _engine(theEngine)
+GEOM_IOperations_i::GEOM_IOperations_i(PortableServer::POA_ptr thePOA,
+                                       GEOM::GEOM_Gen_ptr theEngine,
+                                       ::GEOM_IOperations* theImpl)
+  : SALOME::GenericObj_i( thePOA ), _impl(theImpl), _engine(theEngine)
 {
   thePOA->activate_object(this);
 }
@@ -46,10 +48,8 @@ GEOM_IOperations_i::GEOM_IOperations_i(PortableServer::POA_ptr thePOA, GEOM::GEO
  *  destructor
  */
 //=============================================================================
-
 GEOM_IOperations_i::~GEOM_IOperations_i()
 {}
-
 
 //=============================================================================
 /*!
@@ -60,7 +60,6 @@ CORBA::Boolean GEOM_IOperations_i::IsDone()
 {
   return _impl->IsDone();
 }
-
 
 //=============================================================================
 /*!
@@ -112,10 +111,9 @@ void GEOM_IOperations_i::FinishOperation()
   _impl->FinishOperation();
 }
 
-
 //=============================================================================
 /*!
- *  AboutOperation
+ *  AbortOperation
  */
 //=============================================================================
 void GEOM_IOperations_i::AbortOperation()
@@ -130,9 +128,10 @@ void GEOM_IOperations_i::AbortOperation()
 //=============================================================================
 GEOM::GEOM_Object_ptr GEOM_IOperations_i::GetObject(Handle(GEOM_Object) theObject)
 {
-  if(theObject.IsNull()) return NULL;
+  if (theObject.IsNull()) return NULL;
   TCollection_AsciiString anEntry;
   TDF_Tool::Entry(theObject->GetEntry(), anEntry);
-  GEOM::GEOM_Object_var GO = GEOM::GEOM_Object::_duplicate(_engine->GetObject(theObject->GetDocID(), anEntry.ToCString()));
+  //GEOM::GEOM_Object_var GO = GEOM::GEOM_Object::_duplicate(_engine->GetObject(theObject->GetDocID(), anEntry.ToCString()));
+  GEOM::GEOM_Object_var GO = _engine->GetObject(theObject->GetDocID(), anEntry.ToCString());
   return GO._retn();
 }
