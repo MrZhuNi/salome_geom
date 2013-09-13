64397c2dbf6e13110a2ab98b165af57782524f91
fps <fps@opencascade.com>
2013-09-13 15:59:18 +0000

    add swig for XAO

---
+++ src/XAO_Swig/xao.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from xao_swig import *
+
+#from xao_swig import BOOLEAN, INTEGER, DOUBLE, STRING
+#from xao_swig import VERTEX, EDGE, FACE, SOLID, WHOLE
+#from xao_swig import BREP, STEP
+
+#from xao_swig import VectorBoolean, VectorDouble, VectorInteger, VectorString, VectorStep
+#from xao_swig import VectorVectorBoolean, VectorVectorDouble, VectorVectorInteger, VectorVectorString
+#from xao_swig import ListField, ListGroup, SetInt
+
+#from xao_swig import Field, BooleanField, IntegerField, DoubleField, StringField
+#from xao_swig import Step, BooleanStep, IntegerStep, DoubleStep, StringStep
+#from xao_swig import Xao, Group, Geometry, GeometricElement
+
+#from xao_swig import XAO_Exception
+
+#from xao_swig import BrepGeometry as _BrepGeometry
+
+#class BrepGeometry(_BrepGeometry):
+#    def setShape(self, shape):
+#        if shape is not None and 'GetShapeStream' in dir(shape):
+#            _BrepGeometry.setShape(self, shape.GetShapeStream())
+#        else:
+#            raise XAO_Exception("toto")
+
